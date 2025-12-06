b2f29b28856134018bc2bf978d205aecbe373c57
beavelar <beavelar.github@gmail.com>
2025-12-06 14:43:20 -0700

    Change logger

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"time"
 
 	"github.com/charmbracelet/log"
 	"github.com/google/uuid"
 	_ "github.com/lib/pq"
 )
+
+var logger *log.Logger
 
 const (
 	numUsers           = 1000
@@ -17,43 +20,52 @@ const (
 	maxMessagesPerSeed = 10000
 )
 
+func init() {
+	logger = log.NewWithOptions(os.Stdout, log.Options{
+		TimeFormat:   time.DateTime,
+		Level:        log.DebugLevel,
+		ReportCaller: true,
+		Formatter:    log.JSONFormatter,
+	})
+}
+
 func main() {
 	dbConnStr := os.Getenv("DATABASE_URL")
 	if dbConnStr == "" {
-		log.Fatal("DATABASE_URL environment variable not set. Please set it to your PostgreSQL connection string.")
+		logger.Fatal("DATABASE_URL environment variable not set. Please set it to your PostgreSQL connection string.")
 	}
 
 	db, err := sql.Open("postgres", dbConnStr)
 	if err != nil {
-		log.Fatalf("Error opening database: %v", err)
+		logger.Fatal("Error opening database", "error", err)
 	}
 	defer db.Close()
 
 	err = db.Ping()
 	if err != nil {
-		log.Fatalf("Error connecting to the database: %v", err)
+		logger.Fatal("Error connecting to the database", "error", err)
 	}
-	log.Info("Successfully connected to the database!")
+	logger.Info("Successfully connected to the database!")
 
-	log.Info("Executing init.sql...")
+	logger.Info("Executing init.sql")
 	sqlFile, err := os.ReadFile("init.sql")
 	if err != nil {
-		log.Fatalf("Error reading SQL file: %v", err)
+		logger.Fatal("Error reading SQL file", "error", err)
 	}
 
 	_, err = db.Exec(string(sqlFile))
 	if err != nil {
-		log.Fatalf("Error executing SQL script: %v", err)
+		logger.Fatal("Error executing SQL script", "error", err)
 	}
 
-	log.Info("SQL script (init.sql) executed successfully!")
+	logger.Info("SQL script (init.sql) executed successfully!")
 
-	log.Info("Generating and inserting %d users...\n", numUsers)
+	logger.Info("Generating and inserting users", "numUsers", numUsers)
 	userIDs := make([]uuid.UUID, numUsers)
 
 	stmt, err := db.Prepare("INSERT INTO users (id, username) VALUES ($1, $2)")
 	if err != nil {
-		log.Fatalf("Failed to prepare statement for users: %v", err)
+		logger.Fatal("Failed to prepare statement for users", "error", err)
 	}
 	defer stmt.Close()
 
@@ -62,22 +74,22 @@ func main() {
 		username := fmt.Sprintf("user_%04d", i+1)
 		_, err = stmt.Exec(userID, username)
 		if err != nil {
-			log.Fatalf("Error inserting user %s: %v", username, err)
+			logger.Fatal("Error inserting user", "user", username, "error", err)
 		}
 		userIDs[i] = userID
 	}
 
-	log.Info("%d users inserted successfully!\n", numUsers)
+	logger.Info("Users inserted successfully!", "numUsers", numUsers)
 
 	msgStmt, err := db.Prepare("INSERT INTO messages (user_id, message) VALUES ($1, $2)")
 	if err != nil {
-		log.Fatalf("Failed to prepare statement for messages: %v", err)
+		logger.Fatal("Failed to prepare statement for messages", "error", err)
 	}
 	defer msgStmt.Close()
 
 	for _, user := range userIDs {
 		numMessages := rand.Intn(maxMessagesPerSeed-minMessagesPerSeed+1) + minMessagesPerSeed
-		log.Info("Generating and inserting %d messages for %s...\n", numMessages, user)
+		logger.Info("Generating and inserting messages", "numMessages", numMessages, "user", user)
 
 		for i := range numMessages {
 			randomUserIndex := rand.Intn(numUsers)
@@ -86,10 +98,10 @@ func main() {
 
 			_, err = msgStmt.Exec(userID, messageContent)
 			if err != nil {
-				log.Fatalf("Error inserting message %d: %v", i, err)
+				logger.Fatal("Error inserting message", "messageIdx", i, "error", err)
 			}
 		}
 
-		log.Info("%d messages inserted successfully!\n", numMessages)
+		logger.Info("Messages inserted successfully!", "numMessages", numMessages)
 	}
 }
