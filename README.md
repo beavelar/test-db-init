011824b6df4caf70de7bf671c968a81e2567c717
beavelar <beavelar.github@gmail.com>
2025-12-07 12:52:56 -0700

    Add environment variable and user transactions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Test DB Init
+
+Testing out running DB init scripts on Railway through their "Generate from `GitHub Repo`" option
+
+Main thing of importance is that the build will run on each commit to `main` (or a different branch if a different branch is selected in Railway). Since it runs on each commit, it's important to set some environment variables or some sort of DB versioning with version checks to control when the scripts run or which scripts run if the DB should be versioned
--- main.go
+++ main.go
@@ -30,6 +30,10 @@ func init() {
 }
 
 func main() {
+	if generateMockData := os.Getenv("GENERATE_MOCK_DATA"); generateMockData == "" || generateMockData == "false" {
+		logger.Info("Skipping script", "GENERATE_MOCK_DATA", generateMockData)
+	}
+
 	dbConnStr := os.Getenv("DATABASE_URL")
 	if dbConnStr == "" {
 		logger.Fatal("DATABASE_URL environment variable not set. Please set it to your PostgreSQL connection string.")
@@ -59,49 +63,64 @@ func main() {
 	}
 
 	logger.Info("SQL script (init.sql) executed successfully!")
-
 	logger.Info("Generating and inserting users", "numUsers", numUsers)
-	userIDs := make([]uuid.UUID, numUsers)
 
-	stmt, err := db.Prepare("INSERT INTO users (id, username) VALUES ($1, $2)")
+	userTx, err := db.Begin()
+	if err != nil {
+		logger.Fatal("Failed to start transaction to create users", "error", err)
+	}
+
+	userStmt, err := userTx.Prepare("INSERT INTO users (id, username) VALUES ($1, $2)")
 	if err != nil {
 		logger.Fatal("Failed to prepare statement for users", "error", err)
 	}
-	defer stmt.Close()
+	defer userStmt.Close()
 
+	userIDs := make([]uuid.UUID, numUsers)
 	for i := range numUsers {
 		userID := uuid.New()
 		username := fmt.Sprintf("user_%04d", i+1)
-		_, err = stmt.Exec(userID, username)
+		_, err = userStmt.Exec(userID, username)
 		if err != nil {
 			logger.Fatal("Error inserting user", "user", username, "error", err)
 		}
 		userIDs[i] = userID
 	}
 
+	if err = userTx.Commit(); err != nil {
+		logger.Fatal("Failed to create users", "error", err)
+	}
+
 	logger.Info("Users inserted successfully!", "numUsers", numUsers)
 
-	msgStmt, err := db.Prepare("INSERT INTO messages (user_id, message) VALUES ($1, $2)")
-	if err != nil {
-		logger.Fatal("Failed to prepare statement for messages", "error", err)
-	}
-	defer msgStmt.Close()
-
 	for _, user := range userIDs {
+		msgTx, err := db.Begin()
+		if err != nil {
+			logger.Fatal("Failed to start transaction to create messages", "error", err, "user", user)
+		}
+
+		msgStmt, err := msgTx.Prepare("INSERT INTO messages (user_id, message) VALUES ($1, $2)")
+		if err != nil {
+			logger.Fatal("Failed to prepare statement for messages", "error", err, "user", user)
+		}
+		defer msgStmt.Close()
+
 		numMessages := rand.Intn(maxMessagesPerSeed-minMessagesPerSeed+1) + minMessagesPerSeed
 		logger.Info("Generating and inserting messages", "numMessages", numMessages, "user", user)
 
 		for i := range numMessages {
-			randomUserIndex := rand.Intn(numUsers)
-			userID := userIDs[randomUserIndex]
-			messageContent := fmt.Sprintf("Hello from user %s! This is message number %d.", userID.String()[:8], i+1)
+			messageContent := fmt.Sprintf("Hello from user %s! This is message number %d.", user.String()[:8], i+1)
 
-			_, err = msgStmt.Exec(userID, messageContent)
+			_, err = msgStmt.Exec(user, messageContent)
 			if err != nil {
 				logger.Fatal("Error inserting message", "messageIdx", i, "error", err)
 			}
 		}
 
-		logger.Info("Messages inserted successfully!", "numMessages", numMessages)
+		if err = msgTx.Commit(); err != nil {
+			logger.Fatal("Failed to create messages", "error", err, "user", user)
+		}
+
+		logger.Info("Messages inserted successfully!", "numMessages", numMessages, "user", user)
 	}
 }
